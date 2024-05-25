a67b062c6e18f6cfecfac42c9f985593f2774277
Mahmoud El-laithy <mahmoudelaithy@gmail.com>
2024-05-25 14:23:40 +0300

    Environment support

--- src/test/java/com/Todo/ApiBase/RequestSpec.java
+++ src/test/java/com/Todo/ApiBase/RequestSpec.java
@@ -7,10 +7,29 @@ import static io.restassured.RestAssured.given;
 
 public class RequestSpec {
 
+    public static String getEnv () {
+
+        String env = System.getProperty("env" , "PRODUCTION");
+        String baseURL;
+        switch (env) {
+            case "PRODUCTION":
+                baseURL= "https://todo.qacart.com";
+                break;
+            case "LOCAL":
+                baseURL= "http://localhost:8080";
+                break;
+            default:
+                throw new RuntimeException("Environment is not supported");
+        }
+
+        return baseURL;
+
+    }
+
     public static RequestSpecification getRequestSpec () {
 
         return
-        given().baseUri("https://todo.qacart.com")
+        given().baseUri(getEnv())
                 .contentType(ContentType.JSON)
                 .log().all()
                 ;
